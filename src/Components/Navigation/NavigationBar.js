3e9b51f5fe8e6aa88974f6ca6e4a2ad271e7a6a4
gururaj-kasibhatla <gururajsharmak@gmail.com>
2024-07-25 05:15:23 +0000

    fixed logout functionality

--- src/Components/Navigation/NavigationBar.js
+++ src/Components/Navigation/NavigationBar.js
@@ -31,9 +31,9 @@ const NavigationBar = ({ user, onLogout }) => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+            <button className="nav-link btn btn-link" onClick={() => navigate('/')}>
+                    Home
+                  </button>
             </li>
             {!user ? (
               <>
